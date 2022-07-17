8aa30d0b322ea9fafd6b65f9c93b4218188b4a54
jesse sheehan <jessetsheehan@gmail.com>
2022-07-16 23:13:26 -0400

    added associations, working on routes next

---
+++ models/index.js
@@ -0,0 +1,36 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id'
+})
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: "tag_id",
+  foreignKey: 'product_id'
+})
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_id',
+  foreignKey: "tag_id"
+})
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
